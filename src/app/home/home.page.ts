d8d931a2c327f3d654f6eb6f201fb270e43c1442
Pablo Servin <pablo.s@totvs.com.br>
2021-06-24 09:31:42 -0300

    label

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraSource, CameraResultType } from '@capacitor/camera';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,25 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  async takePicture(){
+
+    const image = await Camera.getPhoto({
+      quality: 50,
+      source: CameraSource.Camera,
+      saveToGallery: false,
+      resultType: CameraResultType.Uri,
+    });
+
+    setTimeout(async () => {
+      const removeFile = await Filesystem.deleteFile({
+        path: image.path,
+      });
+      console.log(removeFile);
+    }, 2500);
+
+    console.log(image);
+
+  }
 
 }
